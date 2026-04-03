099375e7f2cedeb77c6ef4aa55cdc504aa7c99f9
Khushi Sharma <khushisharma1703@gmail.com>
2026-04-03 11:58:05 +0530

    Initial commit

--- ecom-application/src/main/java/com/app/ecom/controller/UserController.java
+++ ecom-application/src/main/java/com/app/ecom/controller/UserController.java
@@ -1,4 +1,6 @@
 package com.app.ecom.controller;
+import com.app.ecom.dto.UserRequest;
+import com.app.ecom.dto.UserResponse;
 import com.app.ecom.model.User;
 import com.app.ecom.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -18,20 +20,20 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping
-    public ResponseEntity<List<User>> getAllUsers() {
+    public ResponseEntity<List<UserResponse>> getAllUsers() {
         return new ResponseEntity<>(userService.fetchAllUsers(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<User> getUser(@PathVariable Long id) {
+    public ResponseEntity<UserResponse> getUser(@PathVariable Long id) {
         return userService.fetchUser(id)
                 .map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @PostMapping
-    public ResponseEntity<String> createUser(@RequestBody User user) {
-        userService.addUser(user);
+    public ResponseEntity<String> createUser(@RequestBody UserRequest userRequest) {
+        userService.addUser(userRequest);
         return ResponseEntity.ok("user added successfully");
     }
 
--- ecom-application/src/main/java/com/app/ecom/service/UserService.java
+++ ecom-application/src/main/java/com/app/ecom/service/UserService.java
@@ -1,5 +1,9 @@
 package com.app.ecom.service;
 
+import com.app.ecom.dto.AddressDTO;
+import com.app.ecom.dto.UserRequest;
+import com.app.ecom.dto.UserResponse;
+import com.app.ecom.model.Address;
 import com.app.ecom.repository.UserRepository;
 import com.app.ecom.model.User;
 import lombok.RequiredArgsConstructor;
@@ -7,6 +11,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -17,20 +22,25 @@ public class UserService {
 
 
 
-    public List<User> fetchAllUsers() {
-        return userRepository.findAll();
+    public List<UserResponse> fetchAllUsers() {
+        return  userRepository.findAll().stream()
+                .map(this::mapToUserResponse)
+                .collect(Collectors.toList());
     }
-    public void addUser(User user){
+    public void addUser(UserRequest userRequest){
        // user.setId(nextId++);
+        User user = new User();
+        updateUserFromRequest(user , userRequest);
          userRepository.save(user);
 
     }
 
-    public Optional<User> fetchUser(Long id) {
+    public Optional<UserResponse> fetchUser(Long id) {
        return userRepository.findById(id);
     }
     public boolean updateUser(Long id, User updateUser ){
         return userRepository.findById(id)
+                .map(this::mapToUserResponse)
 
                 .map(existingUser -> {
                     existingUser.setFirstName(updateUser.getFirstName());
@@ -39,5 +49,36 @@ public class UserService {
                     return true;
                 }).orElse(false);
     }
+    private void updateUserFromRequest(User user, UserRequest userRequest) {
+    user.setFirstName(userRequest.getFirstName());
+    user.setLastName(userRequest.getLastName());
+    user.setEmail(userRequest.getEmail());
+    user.setPhone(user.getPhone());
+    if(userRequest.getAddress()!= null) {
+        Address address = new Address();
+        address
+        }
 
+    }
+private UserResponse mapToUserResponse(User user){
+       UserResponse  response = new UserResponse();
+       response.setId(String.valueOf(user.getId()));
+       response.setFirstName(user.getFirstName());
+       response.setLastName(user.getLastName());
+       response.setEmail(user.getEmail());
+       response.setPhone(user.getPhone());
+       response.setRole(user.getRole());
+
+       if(user.getAddress() != null){
+           AddressDTO addressDTO = new AddressDTO();
+           addressDTO.setStreet(user.getAddress().getStreet());
+           addressDTO.setCity(user.getAddress().getCity());
+           addressDTO.setState(user.getAddress().getState());
+           addressDTO.setCountry(user.getAddress().getCountry());
+           addressDTO.setZipcode(user.getAddress().getZipcode());
+           response.setAddress(addressDTO);
+
+    }
+    return response;
+}
 }
