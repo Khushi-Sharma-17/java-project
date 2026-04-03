4c38f8e36ba15b4d405d7e992cce6536833ea6f5
Khushi Sharma <khushisharma1703@gmail.com>
2026-04-03 10:27:05 +0530

    Initial commit

---
+++ ecom-application/src/main/java/com/app/ecom/service/UserService.java
@@ -0,0 +1,43 @@
+package com.app.ecom.service;
+
+import com.app.ecom.repository.UserRepository;
+import com.app.ecom.model.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+   // private List<User> userList = new ArrayList<>();
+    //private Long nextId = 1L;
+
+
+
+    public List<User> fetchAllUsers() {
+        return userRepository.findAll();
+    }
+    public void addUser(User user){
+       // user.setId(nextId++);
+         userRepository.save(user);
+
+    }
+
+    public Optional<User> fetchUser(Long id) {
+       return userRepository.findById(id);
+    }
+    public boolean updateUser(Long id, User updateUser ){
+        return userRepository.findById(id)
+
+                .map(existingUser -> {
+                    existingUser.setFirstName(updateUser.getFirstName());
+                    existingUser.setLastName(updateUser.getLastName());
+                    userRepository.save(existingUser);
+                    return true;
+                }).orElse(false);
+    }
+
+}
