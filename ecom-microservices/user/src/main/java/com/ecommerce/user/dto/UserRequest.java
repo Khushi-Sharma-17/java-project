b23a30381cc7c56083324c1e53d9e28425fd1dc7
Khushi Sharma <khushisharma1703@gmail.com>
2026-04-23 16:35:47 +0530

    order microservice

---
+++ ecom-microservices/user/src/main/java/com/ecommerce/user/dto/UserRequest.java
@@ -0,0 +1,15 @@
+package com.ecommerce.user.dto;
+
+import lombok.Data;
+import org.antlr.v4.runtime.misc.NotNull;
+
+@Data
+
+public class UserRequest {
+    @NotNull
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private AddressDTO address;
+}
